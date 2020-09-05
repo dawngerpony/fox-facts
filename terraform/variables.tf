a0d9dcd8b53249fad2743f09fefa18ef84971d3a
Dawn James <dawnjames2105@gmail.com>
2020-09-05 18:04:37 +0100

    Remove the 'plan' until we have some credentials.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,26 @@
 variable "credentials" {
   default     = "credentials.json"
   description = "credentials file"
+  type        = string
 }
 
 variable "project" {
   description = "The name of the GCP project"
+  type        = string
 }
 
 variable "region" {
   description = "The GCP region"
+  type        = string
+}
+
+variable "username" {
+  description = "Your GCP username"
+  type        = string
+}
+
+variable "ssh_private_key_filename" {
+  default     = "~/.ssh/id_rsa.pub"
+  description = "The location of your SSH private key"
+  type        = string
 }
