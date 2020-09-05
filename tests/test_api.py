f1e781da4b636ecd938c5f2f6d5cea1596c1a941
Dawn James <dawnjames2105@gmail.com>
2020-09-05 08:47:49 +0100

    Deliberately fail a test to check CI.

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,4 +2,4 @@ from fox_facts.api import FoxFacts
 
 def test_hello_should_return_hello_world():
     fox_facts = FoxFacts()
-    assert fox_facts.get() == {'hello': 'world'}
+    assert fox_facts.get() == {'hello': 'banana'}
